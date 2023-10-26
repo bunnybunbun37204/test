68ed6d097810323b702205849ff6ebd65dcc9013
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2023-10-26 23:10:05 +0700

    test2

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const multer = require('multer');
 const cors = require('cors');
 const app = express();
 const PORT = 4000;
-require('dotenv').config();
 
 const corsOptions = {
     origin: ['http://localhost:3000', 'http://localhost:5000'],
